7cc52fe3120f2d84daef2e4e186f99a5cb69c78d
Valartamizh <valarthamizhrr@gmail.com>
2026-02-25 12:48:51 +0530

    Init

---
+++ src/main/java/com/securin/weather/service/WeatherService.java
@@ -0,0 +1,13 @@
+package com.securin.weather.service;
+
+import com.securin.weather.dto.WeatherDto;
+import java.time.LocalDate;
+import java.util.List;
+
+public interface WeatherService {
+    List<WeatherDto> getWeatherByDate(LocalDate date);
+
+    List<WeatherDto> getWeatherByMonth(int year, int month);
+
+    List<WeatherDto> filterWeather(Double minTemp, Double maxTemp, Integer minHumidity, Integer maxHumidity, int page, int size, String sortBy, String direction);
+}
